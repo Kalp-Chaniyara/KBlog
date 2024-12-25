838b14481ba6ed81b973da73dea2d2a723cf6a70
Kalp-Chaniyara <kalpchaniyara.2006@gmail.com>
2024-12-25 20:00:24 +0530

    setup for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,32 +5,34 @@ import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import postRoutes from "./routes/post.route.js"
+import path from "path";
 
-if(process.env.NODE_ENV !== "production"){
-    dotenv.config({
-        path:"./.env",
-    });
-}
+dotenv.config()
+connectDB();
+const PORT = process.env.PORT || 8080
+const app = e()
 
+const _dirname = path.resolve();
+
+app.use(e.json({ limit: '50mb' }));
+app.use(e.urlencoded({ limit: '50mb', extended: true }));
+app.use(e.json())
+app.use(cookieParser())
 const corsConfig = {
     origin:process.env.CLIENT_URL,
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }
-
-const app = e()
-
-app.use(e.json({ limit: '50mb' }));
-app.use(e.urlencoded({ limit: '50mb', extended: true }));
-
-app.use(e.json())
-app.use(cookieParser())
 app.use(cors(corsConfig))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/post",postRoutes)
 
-app.listen(process.env.PORT,()=>{
-    console.log("server is lsitening on port:" + process.env.PORT);
-    connectDB();
+app.use(e.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+})
+
+app.listen(PORT,()=>{
+    console.log("server is lsitening on port:" + PORT);
 })
